5204c2a150bc78f0b605c43d0fd4478d1e8efec0
Irfan Ardiansyah <ardiafire27@gmail.com>
2022-07-25 23:31:05 +0700

    add chart n log

--- resources/views/student_courses/code_test.blade.php
+++ resources/views/student_courses/code_test.blade.php
@@ -19,24 +19,53 @@
   </style>
   <div style="margin-top: 70px">
     <div class="row">
-      <div class="col-md-7" style="min-height: 100vh">
-        <div class="editor"></div>
-      </div>
+      @if (!$is_finish)
+        <div class="col-md-7" style="min-height: 100vh">
+          <div class="editor"></div>
+        </div>
+      @endif
+
+      @if ($is_finish)
+        <div class="col-md-7 p-5">
+          <h3>Error Logs ({{ $error_logs->count() }})</h3>
+          <div>
+            @foreach ($error_logs as $i => $err)
+              <div class="card card-sm">
+                <div class="card-body">
+                  <span class="badge badge-danger mb-2"> Error {{ $i + 1 }}</span><br />
+                  <pre>{{ $err->error_message }}</pre>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        </div>
+      @endif
 
       <div class="col-md-5 p-5">
         <div>
           <h3 class="card-title">Code Test</h3>
           {!! $question->question !!}
         </div><br />
-        <div>
-          Run Output<br />
-          <pre id="output"></pre>
-        </div>
+        @if (!$is_finish)
+          <div>
+            Run Output<br />
+            <pre id="output"></pre>
+          </div>
+        @else
+          <div>
+            <span class="badge badge-info">
+              Duration from {{ $user_score->started_at }} to
+              {{ $user_score->ended_at }}
+            </span>
+          </div>
+        @endif
+
       </div>
     </div>
   </div>
 
-  <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModal" aria-hidden="true">
+  <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModal"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -105,12 +134,37 @@
       }).done(function(data) {
         $('#output').text(`${data.output.java}\n${data.output.test_output}`);
         $('#score').val(data.output.point || 0);
+
+        if (data.output.point === 0 || data.output.point === undefined) {
+          send_err_code(data.output)
+        } else {
+          submitCode();
+        }
       }).fail(function(data, err) {
         alert("fail " + JSON.stringify(data) + " " + JSON.stringify(err));
       });
     }
 
-
+    function send_err_code(err) {
+      var user_id = $("#user_id").val();
+      var question_id = $("#question_id").val();
+      var err = err.java + "" + err.test_output;
+      var data = {
+        user_id: user_id,
+        question_id: question_id,
+        error_message: err
+      }
+      $.ajax({
+        url: "/api/questions/error_code_log/create",
+        type: "POST",
+        data: data
+      }).done(function(data) {
+        console.log(data);
+      }).fail(function(data, err) {
+        console.log(data);
+        console.log(err);
+      });
+    }
 
     var is_finish = "{{ $is_finish }}";
 
@@ -120,6 +174,7 @@
       if (cs == null || cs == undefined || cs !== window.location.href) {
         localStorage.setItem("code_session", window.location.href);
         localStorage.setItem("time_end", target)
+        localStorage.setItem("start_time", moment().format("YYYY-MM-DD HH:mm:ss"));
       } else {
         var te = localStorage.getItem("time_end")
         target = moment(te);
@@ -148,6 +203,8 @@
       var course_id = $("#course_id").val();
       var score = $("#score").val();
       var _token = document.getElementsByName("_token")[0].value;
+      var start_time = localStorage.getItem("start_time");
+      var end_time = moment().format("YYYY-MM-DD HH:mm:ss");
 
       $.ajax({
         url: "{{ route('code_test.submit', [$question->id]) }}",
@@ -158,6 +215,8 @@
           content_id: content_id,
           course_id: course_id,
           score: score,
+          started_at: start_time,
+          ended_at: end_time,
           _token: _token
         }
       }).done(res => {
@@ -174,6 +233,8 @@
       var course_id = $("#course_id").val();
       var score = $("#score").val();
       var _token = document.getElementsByName("_token")[0].value;
+      var start_time = localStorage.getItem("start_time");
+      var end_time = moment().format("YYYY-MM-DD HH:mm:ss");
 
       $.ajax({
         url: "{{ route('code_test.submit', [$question->id]) }}",
@@ -184,6 +245,8 @@
           content_id: content_id,
           course_id: course_id,
           score: score,
+          started_at: start_time,
+          ended_at: end_time,
           _token: _token
         }
       });
